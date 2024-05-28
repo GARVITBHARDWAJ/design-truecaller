f3d6fd543aefd22f9a51f37377f4a9e168d1b080
Garvit Bhardwaj <garvitbhardwaj06022000@gmail.com>
2024-05-28 23:57:04 +0530

    Initial Commit

---
+++ src/main/java/com/example/truecaller/TruecallerApplication.java
@@ -0,0 +1,9 @@
+package com.example.truecaller;
+
+public class TruecallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruecallerApplication.class, args);
+	}
+
+}
